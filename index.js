a81e72312e0adf60af93666b89d60cdde4c971d1
edgarrios412 <edgarrios412@gmail.com>
2023-04-10 17:09:40 -0500

    Pushh

--- index.js
+++ index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = 3001;
+const PORT = process.env.PORT || 4000;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
-    console.log('%s listening at '+ port); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at '+ PORT); // eslint-disable-line no-console
   });
 });
